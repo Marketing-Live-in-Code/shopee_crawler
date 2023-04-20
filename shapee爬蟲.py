53302011f382a60876cfae4ab9353246bb6e4ead
Ivan Yang <ivanyang0606@gmail.com>
2023-04-20 16:26:06 +0800

    因蝦皮要求強制登入，且防爬蟲機制越來越嚴格，因此大修整個爬蟲的邏輯與模式
    因蝦皮要求強制登入，且防爬蟲機制越來越嚴格，因此大修整個爬蟲的邏輯與模式

--- shapee爬蟲.py
+++ shapee爬蟲.py
@@ -10,6 +10,7 @@ shapee爬蟲
 更新紀錄：
 2022/09/17 selenium將套件更新到4.4.3版本，因此寫法全部都更新過
 2022/12/29 蝦皮API加入防爬蟲機制，因此改用seleniumwire撈取封包進行爬取
+2023/4/20 因蝦皮要求強制登入，且防爬蟲機制越來越嚴格，因此大修整個爬蟲的邏輯與模式
 """
 
 import requests
@@ -23,9 +24,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 import re
 import random
 import zlib
+from tqdm import tqdm
+from bs4 import BeautifulSoup
 
-keyword = '花襯衫'
-page = 3
+keyword = '運動內衣'
+page = 60
 ecode = 'utf-8-sig'
 
 # 2022/11/21 由於蝦皮API更新，商品細節資料無法再單純使用request爬取，因此header只留給爬留言使用，而流言的API沒什麼檢查
@@ -82,46 +85,25 @@ options.add_argument('blink-settings=imagesEnabled=false')
 driver = webdriver.Chrome(service=service, chrome_options=options)
 time.sleep(random.randint(5,10))
 
-# 開啟網頁
-driver.get('https://shopee.tw/search?keyword=' + keyword )
-time.sleep(random.randint(10,20))
+# 開啟網頁，進到首頁
+driver.get('https://shopee.tw/' )
+time.sleep(random.randint(5,10))
+
+####################################################################################
+###                       至此，請先停下來手動登入帳號，再往後執行                  ###
+####################################################################################
 
 
+#---------- Part 1. 主要先抓下商品名稱與連結，之後再慢慢補上詳細資料 ----------
 print('---------- 開始進行爬蟲 ----------')
 tStart = time.time()#計時開始
-
-container_product = pd.DataFrame()
-container_comment = pd.DataFrame()
-for i in range(int(page)):
-    # 準備用來存放資料的陣列
-    itemid = []
-    shopid =[]
-    name = []
-    link = []
-    brand = []
-    stock = []
-    price = []
-    ctime = []
-    description = []
-    discount = []
-    can_use_bundle_deal = []
-    can_use_wholesale = []
-    tier_variations = []
-    historical_sold = []
-    is_cc_installment_payment_eligible = []
-    is_official_shop = []
-    is_pre_order = []
-    liked_count = []
-    shop_location = []
-    SKU = []
-    cmt_count = []
-    five_star = []
-    four_star = []
-    three_star = []
-    two_star = []
-    one_star = []
-    rating_star = []
-    
+# 準備用來存放資料的陣列
+itemid = []
+shopid =[]
+name = []
+link = []
+price = []
+for i in tqdm(range(int(page))):
     driver.get('https://shopee.tw/search?keyword=' + keyword + '&page=' + str(i))
     time.sleep(random.randint(5,10))
     # 滾動頁面
@@ -129,206 +111,227 @@ for i in range(int(page)):
         driver.execute_script('window.scrollBy(0,1000)')
         time.sleep(random.randint(3,10))
     
-    #取得商品內容
-    for item, thename in zip(driver.find_elements(by=By.XPATH, value='//*[@data-sqe="link"]'), driver.find_elements(by=By.XPATH, value='//*[@data-sqe="name"]')):
-        #商品ID、商家ID、商品連結
-        getID = item.get_attribute('href')
+    # 2023/04/20 由於使用selenium取得商品有些不穩定，因此以下換成全部使用bs4去解析
+    # 取得商品內容
+    for block in driver.find_elements(by=By.XPATH, value='//*[@data-sqe="item"]'):
+        # 將整個網站的Html進行解析
+        soup = BeautifulSoup(block.get_attribute('innerHTML'), "html.parser").find('a')
+        # 商品ID、商家ID、商品連結
+        getID = soup.get('href')
         theitemid = int((getID[getID.rfind('.')+1:getID.rfind('?')]))
         theshopid = int(getID[ getID[:getID.rfind('.')].rfind('.')+1 :getID.rfind('.')]) 
-        link.append(getID)
-        itemid.append(theitemid)
-        shopid.append(theshopid)
         
-        #商品名稱
-        getname = thename.text.split('\n')[0]
-        name.append(getname)
+        # 先整理標籤
+        get_parent = soup.find('div',{"data-sqe":"name"}).parent.find_all("div", recursive=False)
         
-        #價格
-        thecontent = item.text
-        thecontent = thecontent[(thecontent.find(getname)) + len(getname):]
-        thecontent = thecontent.replace('萬','000')
-        thecut = thecontent.split('\n')
-
-        if bool(re.search('市|區|縣|鄉|海外|中國大陸', thecontent)): #有時會沒有商品地點資料
-            if bool(re.search('已售出', thecontent)): #有時會沒銷售資料
-                if '出售' in thecut[-3][1:]:
-                    theprice = thecut[-4][1:]
-                else:
-                    theprice = thecut[-3][1:]
-
-            else:
-                theprice = thecut[-2][1:]
+        # 商品名稱
+        if len(get_parent) >0: # 確認有資料再進行
+            getname = get_parent[0].find('div').text
         else:
-            if re.search('已售出', thecontent): #有時會沒銷售資料
-                theprice = thecut[-2][1:]
-            else:
-                theprice = thecut[-1][1:]
-                
-        theprice = theprice.replace('$','')
-        theprice = theprice.replace(',','')
-        theprice = theprice.replace('售','')
-        theprice = theprice.replace('出','')
-        theprice = theprice.replace(' ','')
-        if ' - ' in theprice:
-            theprice = (int(theprice.split(' - ')[0]) +int(theprice.split(' - ')[1]))/2
-        if '-' in theprice:
-            theprice = (int(theprice.split('-')[0]) +int(theprice.split('-')[1]))/2
-        price.append(int(theprice))
+            print('抓不到資料，直接是空的')
+            continue # 沒抓到這個商品就別爬了
 
-    for lnk, itid, shid in zip(link, itemid, shopid):
-        #請求商品詳細資料
-        itemDetail = goods_detail(url = lnk, item_id = itid, shop_id = shid)
-        print('抓取： '+ str(itid) )
-        if itemDetail == '':
-            # 抓不到資料就全部塞空值
-            print('抓不到商品詳細資料...\n')
-            brand.append(None) #品牌
-            stock.append(None) #存貨數量
-            ctime.append(None) #上架時間
-            description.append(None) #商品文案
-            discount.append(None) #折數
-            can_use_bundle_deal.append(None) #可否搭配購買
-            can_use_wholesale.append(None) #可否大量批貨購買
-            tier_variations.append(None) #選項
-            historical_sold.append(None) #歷史銷售量
-            is_cc_installment_payment_eligible.append(None) #可否分期付款
-            is_official_shop.append(None) #是否官方賣家帳號
-            is_pre_order.append(None) #是否可預購
-            liked_count.append(None) #喜愛數量
-            SKU.append(None) #SKU
-            shop_location.append(None) #商家地點
-            cmt_count.append(None) #評價數量
-            five_star.append(None) #五星
-            four_star.append(None) #四星
-            three_star.append(None) #三星
-            two_star.append(None) #二星
-            one_star.append(None) #一星
-            rating_star.append(None) #評分
-            continue
+        #價格
+        if len(get_parent) >1: # 確認有資料再進行
+            getSpan = get_parent[1].find_all('span')
+            counter = []
+            for j in getSpan:
+                theprice = j.text
+                theprice = theprice.replace('萬','')
+                theprice = theprice.replace('$','')
+                theprice = theprice.replace(',','')
+                theprice = theprice.replace(' ','')
 
-        brand.append(itemDetail['brand']) #品牌
-        stock.append(itemDetail['stock']) #存貨數量
-        ctime.append(itemDetail['ctime']) #上架時間
-        description.append(itemDetail['description']) #商品文案
-        discount.append(itemDetail['discount']) #折數
-        can_use_bundle_deal.append(itemDetail['can_use_bundle_deal']) #可否搭配購買
-        can_use_wholesale.append(itemDetail['can_use_wholesale']) #可否大量批貨購買
-        tier_variations.append(itemDetail['tier_variations']) #選項
-        historical_sold.append(itemDetail['historical_sold']) #歷史銷售量
-        is_cc_installment_payment_eligible.append(itemDetail['is_cc_installment_payment_eligible']) #可否分期付款
-        is_official_shop.append(itemDetail['is_official_shop']) #是否官方賣家帳號
-        is_pre_order.append(itemDetail['is_pre_order']) #是否可預購
-        liked_count.append(itemDetail['liked_count']) #喜愛數量
-        
-        #SKU
-        all_sku=[]
-        for sk in itemDetail['models']:
-            all_sku.append(sk['name'])
-        SKU.append(all_sku) #SKU
-        shop_location.append(itemDetail['shop_location']) #商家地點
-        cmt_count.append(itemDetail['cmt_count']) #評價數量
-        five_star.append( itemDetail['item_rating']['rating_count'][5] ) #五星
-        four_star.append( itemDetail['item_rating']['rating_count'][4] ) #四星
-        three_star.append( itemDetail['item_rating']['rating_count'][3] ) #三星
-        two_star.append( itemDetail['item_rating']['rating_count'][2] ) #二星
-        one_star.append( itemDetail['item_rating']['rating_count'][1] ) #一星
-        rating_star.append(itemDetail['item_rating']['rating_star']) #評分
-        
-        # 消費者評論詳細資料
-        iteComment = goods_comments(item_id = theitemid, shop_id = theshopid)
-        userid = [] #使用者ID
-        anonymous = [] #是否匿名
-        commentTime = [] #留言時間
-        is_hidden = [] #是否隱藏
-        orderid = [] #訂單編號
-        comment_rating_star = [] #給星
-        comment = [] #留言內容
-        product_SKU = [] #商品規格
-        
-        for comm in iteComment:
-            userid.append(comm['userid'])
-            anonymous.append(comm['anonymous'])
-            commentTime.append(comm['ctime'])
-            is_hidden.append(comm['is_hidden'])
-            orderid.append(comm['orderid'])
-            comment_rating_star.append(comm['rating_star'])
-            try:
-                comment.append(comm['comment'])
-            except:
-                comment.append(None)
-            
-            p=[]
-            for pro in comm['product_items']:
-                try:
-                    p.append(pro['model_name'])
-                except:
-                    p.append(None)
-                    
-            product_SKU.append(p)
-            
-        commDic = {
-            '商品ID':[ theitemid for x in range(len(iteComment)) ],
-            '賣家ID':[ theshopid for x in range(len(iteComment)) ],
-            '商品名稱':[ getname for x in range(len(iteComment)) ],
-            '價格':[ int(theprice) for x in range(len(iteComment)) ],
-            '使用者ID':userid,
-            '是否匿名':anonymous,
-            '留言時間':commentTime,
-            '是否隱藏':is_hidden,
-            '訂單編號':orderid,
-            '給星':comment_rating_star,
-            '留言內容':comment,
-            '商品規格':product_SKU
-            }
+                if theprice != '':
+                    counter.append(int(theprice))
 
-        container_comment = pd.concat([container_comment,pd.DataFrame(commDic)], axis=0)
-        time.sleep(random.randint(20,150)) # 休息久一點
-        
-    dic = {
-            '商品ID':itemid,
-            '賣家ID':shopid,
-            '商品名稱':name,
-            '商品連結':link,
-            '品牌':brand,
-            '存貨數量':stock,
-            '價格':price,
-            '商品文案':description,
-            '折數':discount,
-            '可否搭配購買':can_use_bundle_deal,
-            '可否大量批貨購買':can_use_wholesale,
-            '選項':tier_variations,
-            '歷史銷售量':historical_sold,
-            '可否分期付款':is_cc_installment_payment_eligible,
-            '是否官方賣家帳號':is_official_shop,
-            '是否可預購':is_pre_order,
-            '喜愛數量':liked_count,
-            '商家地點':shop_location,
-            'SKU':SKU,
-            '評價數量':cmt_count,
-            '五星':five_star,
-            '四星':four_star,
-            '三星':three_star,
-            '二星':two_star,
-            '一星':one_star,
-            '評分':rating_star,
-            }
+            # 到這邊確認都有抓到資料，才將它塞入陣列，否則可能會有缺漏
+            link.append("https://shopee.tw"+getID)
+            itemid.append(theitemid)
+            shopid.append(theshopid)
+            name.append(getname)
+            price.append(sum(counter)/len(counter))
+        else:
+            print('抓不到價格資料')
+            continue # 沒抓到這個商品就別爬了
 
-    #資料整合
-    container_product = pd.concat([container_product,pd.DataFrame(dic)], axis=0)
-    #暫時存檔紀錄
-    container_product.to_csv('shopeeAPIData'+str(i+1)+'_Product.csv', encoding = ecode, index=False)
-    container_comment.to_csv('shopeeAPIData'+str(i+1)+'_Comment.csv', encoding = ecode, index=False)
 
-    print('目前累積商品： ' + str(len(container_product)) + ' 留言累積' + str(len(container_comment)))
-    time.sleep(random.randint(20,150)) # 休息久一點
+    time.sleep(random.randint(20,30)) # 休息久一點
 
-container_product.to_csv(keyword +'_商品資料.csv', encoding = ecode, index=False)
-container_comment.to_csv(keyword +'_留言資料.csv', encoding = ecode, index=False)
+# 2023/04/20 先將每頁抓到的商品儲存下來，方便後續追蹤並爬蟲
+dic = {
+    '商品ID':itemid,
+    '賣家ID':shopid,
+    '商品名稱':name,
+    '商品連結':link,
+    '價格':price,
+    '品牌': [ None for x in range(len(itemid)) ] ,
+    '存貨數量':[ None for x in range(len(itemid)) ] ,
+    '商品文案':[ None for x in range(len(itemid)) ] ,
+    '上架時間':[ None for x in range(len(itemid)) ] ,
+    '折數':[ None for x in range(len(itemid)) ] ,
+    '可否搭配購買':[ None for x in range(len(itemid)) ] ,
+    '可否大量批貨購買':[ None for x in range(len(itemid)) ] ,
+    '選項':[ None for x in range(len(itemid)) ] ,
+    '歷史銷售量':[ None for x in range(len(itemid)) ] ,
+    '可否分期付款':[ None for x in range(len(itemid)) ] ,
+    '是否官方賣家帳號':[ None for x in range(len(itemid)) ] ,
+    '是否可預購':[ None for x in range(len(itemid)) ] ,
+    '喜愛數量':[ None for x in range(len(itemid)) ] ,
+    '商家地點':[ None for x in range(len(itemid)) ] ,
+    'SKU':[ None for x in range(len(itemid)) ] ,
+    '評價數量':[ None for x in range(len(itemid)) ] ,
+    '五星':[ None for x in range(len(itemid)) ] ,
+    '四星':[ None for x in range(len(itemid)) ] ,
+    '三星':[ None for x in range(len(itemid)) ] ,
+    '二星':[ None for x in range(len(itemid)) ] ,
+    '一星':[ None for x in range(len(itemid)) ] ,
+    '評分':[ None for x in range(len(itemid)) ] ,
+    '資料已完整爬取':[ False for x in range(len(itemid)) ] ,
+}
+pd.DataFrame(dic).to_csv(keyword +'_商品資料.csv', encoding = ecode, index=False)
 
 tEnd = time.time()#計時結束
 totalTime = int(tEnd - tStart)
 minute = totalTime // 60
 second = totalTime % 60
 print('資料儲存完成，花費時間（約）： ' + str(minute) + ' 分 ' + str(second) + '秒')
+
+
+
+
+
+#---------- Part 2. 補上商品的詳細資料，由於多設了爬取的標記，因此爬過的就不會再爬了 ----------
+print('---------- 開始進行爬蟲 ----------')
+tStart = time.time()#計時開始
+
+# 2023/04/20 先取得之前爬下來的紀錄
+getData = pd.read_csv(keyword +'_商品資料.csv')
+for i in tqdm(range(len(getData))):
+    data = []
+    # 2023/04/20 備標註已經抓過的，就不用再抓了，這樣就算之前爬到一半被中斷，也不會努力付諸東流
+    if getData.iloc[i]['資料已完整爬取']==True:
+        continue
+    data.append(getData.iloc[i]['商品ID'])
+    data.append(getData.iloc[i]['賣家ID'])
+    data.append(getData.iloc[i]['商品名稱'])
+    data.append(getData.iloc[i]['商品連結'])
+    data.append(getData.iloc[i]['價格'])
+    #請求商品詳細資料
+    itemDetail = goods_detail(url = data[3], item_id = data[0], shop_id = data[1])
+
+    # 抓不到資料就先跳過
+    if itemDetail == '':
+        print('抓不到商品詳細資料...\n')
+        continue
+
+    data.append(itemDetail['brand'])# 品牌
+    data.append(itemDetail['stock'])# 存貨數量
+    data.append(itemDetail['description'])# 商品文案
+    data.append(itemDetail['ctime'])# 上架時間
+    data.append(itemDetail['discount'])# 折數
+    data.append(itemDetail['can_use_bundle_deal'])# 可否搭配購買
+    data.append(itemDetail['can_use_wholesale'])# 可否大量批貨購買
+    data.append(itemDetail['tier_variations'])# 可否分期付款
+    data.append(itemDetail['historical_sold'])# 歷史銷售量
+    data.append(itemDetail['is_cc_installment_payment_eligible'])# 可否分期付款
+    data.append(itemDetail['is_official_shop'])# 是否官方賣家帳號
+    data.append(itemDetail['is_pre_order'])# 是否可預購
+    data.append(itemDetail['liked_count'])# 喜愛數量
+    data.append(itemDetail['shop_location'])# 商家地點
+    #SKU
+    all_sku=[]
+    for sk in itemDetail['models']:
+        all_sku.append(sk['name'])
+    data.append(all_sku)# SKU
+    data.append(itemDetail['cmt_count'])# 評價數量
+    data.append(itemDetail['item_rating']['rating_count'][5])# 五星
+    data.append(itemDetail['item_rating']['rating_count'][4])# 四星
+    data.append(itemDetail['item_rating']['rating_count'][3])# 三星
+    data.append(itemDetail['item_rating']['rating_count'][2])# 二星
+    data.append(itemDetail['item_rating']['rating_count'][1])# 一星
+    data.append(itemDetail['item_rating']['rating_star'])# 評分
+    data.append(True)# 資料已完整爬取
+
+    getData.iloc[i] = data #塞入所有資料
+    getData.to_csv(keyword +'_商品資料.csv', encoding = ecode, index=False)
+    
+    time.sleep(random.randint(45,70)) # 休息久一點
+
+    # 每爬5個商品，會再有一次更長的休息
+    if i%5 == 0 :
+        time.sleep(random.randint(30,150)) 
+
+tEnd = time.time()#計時結束
+totalTime = int(tEnd - tStart)
+minute = totalTime // 60
+second = totalTime % 60
+print('資料儲存完成，花費時間（約）： ' + str(minute) + ' 分 ' + str(second) + '秒')
+
+
+
+#---------- Part 3. 補上留言資料 ----------
+print('---------- 開始進行爬蟲 ----------')
+tStart = time.time()#計時開始
+container_comment = pd.DataFrame()
+getData = pd.read_csv(keyword +'_留言資料.csv')
+for i in range(len(getData)):
+    
+    # 消費者評論詳細資料
+    iteComment = goods_comments(item_id = theitemid, shop_id = theshopid)
+    userid = [] #使用者ID
+    anonymous = [] #是否匿名
+    commentTime = [] #留言時間
+    is_hidden = [] #是否隱藏
+    orderid = [] #訂單編號
+    comment_rating_star = [] #給星
+    comment = [] #留言內容
+    product_SKU = [] #商品規格
+    
+    for comm in iteComment:
+        userid.append(comm['userid'])
+        anonymous.append(comm['anonymous'])
+        commentTime.append(comm['ctime'])
+        is_hidden.append(comm['is_hidden'])
+        orderid.append(comm['orderid'])
+        comment_rating_star.append(comm['rating_star'])
+        try:
+            comment.append(comm['comment'])
+        except:
+            comment.append(None)
+        
+        p=[]
+        for pro in comm['product_items']:
+            try:
+                p.append(pro['model_name'])
+            except:
+                p.append(None)
+                
+        product_SKU.append(p)
+        
+    commDic = {
+        '商品ID':[ theitemid for x in range(len(iteComment)) ],
+        '賣家ID':[ theshopid for x in range(len(iteComment)) ],
+        '商品名稱':[ getname for x in range(len(iteComment)) ],
+        '價格':[ int(theprice) for x in range(len(iteComment)) ],
+        '使用者ID':userid,
+        '是否匿名':anonymous,
+        '留言時間':commentTime,
+        '是否隱藏':is_hidden,
+        '訂單編號':orderid,
+        '給星':comment_rating_star,
+        '留言內容':comment,
+        '商品規格':product_SKU
+        }
+
+    container_comment = pd.concat([container_comment,pd.DataFrame(commDic)], axis=0)
+    container_comment.to_csv(keyword +'_留言資料.csv', encoding = ecode, index=False)
+
+tEnd = time.time()#計時結束
+totalTime = int(tEnd - tStart)
+minute = totalTime // 60
+second = totalTime % 60
+print('資料儲存完成，花費時間（約）： ' + str(minute) + ' 分 ' + str(second) + '秒')
+
 
 driver.close() 
